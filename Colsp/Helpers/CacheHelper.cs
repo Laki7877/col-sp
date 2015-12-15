fde0a5f66c780667758deb4c43fdb49ecaadc2cb
Poon Wu <poon.wuthi@gmail.com>
2015-12-15 13:39:13 +0700

    Added CacheHelper

---
+++ Colsp/Helpers/CacheHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Runtime.Caching;
+
+namespace Colsp.Helpers
+{
+	public static class CacheHelper
+	{
+		public static object GetValue(string key)
+		{
+			var memoryCache = MemoryCache.Default;
+			return memoryCache.Get(key);
+		}
+
+		public static bool Add(string key, object value)
+		{
+			var memoryCache = MemoryCache.Default;
+			return memoryCache.Add(key, value, DateTimeOffset.UtcNow.AddMinutes(ConfigHelper.Settings.CacheExpiration.Value));
+		}
+
+		public static void Delete(string key)
+		{
+			var memoryCache = MemoryCache.Default;
+			if (memoryCache.Contains(key))
+			{
+				memoryCache.Remove(key);
+			}
+		}
+	}
+}
