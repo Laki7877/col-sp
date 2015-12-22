cb11b615ac1a16dcd234da0b40c1d53b64c254a9
Poon Wu <poon.wuthi@gmail.com>
2015-12-22 18:48:41 +0700

    Updated Entities

---
+++ Colsp.Api.Models/Requests/UserRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Colsp.Api.Models
+{
+	public class UserRequest : PaginatedRequest
+	{
+		public string Name { get; set; }
+		public int ShopId { get; set; }
+	}
+}
