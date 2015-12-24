475379603123ca017749cdd5aaf9ea3c85d00b66
poonwu <poon.wuthi@gmail.com>
2015-12-24 17:25:59 +0700

    Added UserPrincipal as a custom User principal

---
+++ Colsp.Api/Extensions/IPrincipalExtension.cs
@@ -0,0 +1,30 @@
+﻿using Colsp.Api.Security;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Security.Principal;
+using System.Web;
+
+namespace Colsp.Api.Extensions
+{
+	public static class IPrincipalExtension
+	{
+		public static bool HasPermission(this IPrincipal p, string permission)
+		{
+			if(p is ClaimsPrincipal)
+			{
+				return ((ClaimsPrincipal)p).HasClaim("Permission", permission);
+			}
+			return false;
+		}
+		public static int? UserId(this IPrincipal p)
+		{
+			if(p is UsersPrincipal)
+			{
+				return ((UsersPrincipal)p).UserId;
+			}
+			return null;
+		}
+	}
+}
---
+++ Colsp.Api/Security/UsersPrincipal.cs
@@ -0,0 +1,20 @@
+﻿using System.Security.Claims;
+using System.Security.Principal;
+
+namespace Colsp.Api.Security
+{
+	public class UsersPrincipal : ClaimsPrincipal
+	{
+		public int UserId { get; set; }
+		public string NameEn { get; set; }
+		public string NameTh { get; set; }
+		public string Email { get; set; }
+		public UsersPrincipal(IIdentity identity, int userId, string nameEn, string nameTh, string email) : base(identity)
+		{
+			UserId = userId;
+			NameEn = nameEn;
+			NameTh = nameTh;
+			Email = email;
+		}
+	}
+}
