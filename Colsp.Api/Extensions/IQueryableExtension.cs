fa11fd2865465672c7aa6fc4c5b1501a5999e28b
poonwu <poon.wuthi@gmail.com>
2015-12-24 17:24:39 +0700

    Changed Query service to extensions

---
+++ Colsp.Api/Extensions/IQueryableExtension.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Linq.Dynamic;
+using Colsp.Model.Requests;
+
+namespace Colsp.Api.Extensions
+{
+	public static class IQueryableExtension
+	{
+		public static IQueryable<T> Paginate<T>(this IQueryable<T> iq, PaginatedRequest pagination)
+		{
+			//Generate Lambda Expression for LINQ that will sort
+			//It is equivalent to doing OrderBy(p => p.Name) which will order by Name,
+			//but dynamically at runtime
+			var _order = pagination._order; 
+			var _dir = pagination._direction.ToUpper().Equals("DESC") ? "DESC" : "ASC";
+			iq = iq.OrderBy(_order + " " + _dir);
+				
+			return iq.Skip((int)pagination._offset).Take((int)pagination._limit);
+		}
+	}
+}
