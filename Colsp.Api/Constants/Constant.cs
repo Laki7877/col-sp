86e63adc2fde85f2e52342443efccb1b782e242d
ThanakritW <thanakrit3107@gmail.com>
2016-02-24 14:21:06 +0700

    recommit constant

--- Colsp.Api/Constants/Constant.cs
+++ Colsp.Api/Constants/Constant.cs
@@ -33,6 +33,20 @@ namespace Colsp.Api.Constants
 
         public static readonly string COUPON_FILTER_INCLUDE = "I";
         public static readonly string COUPON_FILTER_EXCLUDE = "E";
+               
+
+        #region CMS
+        public static readonly int CMS_SHOP_GOBAL = 0;
+
+        public static readonly int CMS_STATUS_DRAFT = 1;
+        public static readonly int CMS_STATUS_WAIT_FOR_APPROVAL = 4;
+        public static readonly int CMS_STATUS_JUNK = 5;
+        public static readonly int CMS_STATUS_APPROVE = 2;
+        public static readonly int CMS_STATUS_NOT_APPROVE = 3;
+
+        public static readonly int CMS_TYPE_STATIC_PAGE = 1;
+        public static readonly int CMS_TYPE_COLLECTION_PAGE = 2;
+        #endregion
 
         public static int SHOP_OWNER_GROUP_ID = 2;
 
