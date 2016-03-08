6f69778dcf6f137a96c4e6cca74e594145f4bd5b
thanakrit <wathanakrit@col.co.th>
2016-03-08 13:51:33 +0700

    cs project file

---
+++ Colsp.Api/Constants/Constant_BACKUP_2732.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Colsp.Api.Constants
+{
+    public static class Constant
+    {
+        public static readonly string STATUS_ACTIVE = "AT";
+        public static readonly string STATUS_NOT_ACTIVE = "NA";
+        public static readonly string STATUS_REMOVE = "RM";
+        public static readonly string STATUS_VISIBLE = "VI";
+        public static readonly string STATUS_NOT_VISIBLE = "NV";
+
+        public static readonly string DATA_TYPE_STRING = "ST";
+        public static readonly string DATA_TYPE_LIST = "LT";
+        public static readonly string DATA_TYPE_CHECKBOX = "CB";
+
+        public static Dictionary<string, int?> STOCK_TYPE = new Dictionary<string, int?>() { { "Stock", 1 }, { "Pre-Order", 2 } };
+
+
+        public static readonly string PRODUCT_STATUS_DRAFT = "DF";
+        public static readonly string PRODUCT_STATUS_WAIT_FOR_APPROVAL = "WA";
+        public static readonly string PRODUCT_STATUS_JUNK = "JU";
+        public static readonly string PRODUCT_STATUS_APPROVE = "AP";
+        public static readonly string PRODUCT_STATUS_NOT_APPROVE = "RJ";
+
+
+        public static readonly string USER_TYPE_ADMIN = "A";
+        public static readonly string USER_TYPE_SELLER = "S";
+        public static readonly string SHOP_TYPE = "H";
+
+
+        public static readonly string COUPON_FILTER_INCLUDE = "I";
+        public static readonly string COUPON_FILTER_EXCLUDE = "E";
+
+<<<<<<< HEAD
+
+        #region CMS
+        public static readonly int CMS_SHOP_GOBAL = 0;
+
+        public static readonly int CMS_STATUS_DRAFT = 1;
+        public static readonly int CMS_STATUS_WAIT_FOR_APPROVAL = 4;
+        public static readonly int CMS_STATUS_JUNK = 5;
+        public static readonly int CMS_STATUS_APPROVE = 2;
+        public static readonly int CMS_STATUS_NOT_APPROVE = 3;
+
+        public static readonly int CMS_TYPE_STATIC_PAGE = 1;
+        public static readonly int CMS_TYPE_COLLECTION_PAGE = 2;
+        #endregion
+
+        public static int SHOP_OWNER_GROUP_ID = 2;
+
+        public static string SHOP_GROUP_BU = "BU";
+        public static string SHOP_GROUP_INDY = "IN";
+        public static string SHOP_GROUP_SELLER = "SE";
+
+        public static string STATUS_PROMOTION_ACTIVE = "AT";
+        public static string STATUS_PROMOTION_INACTIVE = "IA";
+=======
+        public static  int SHOP_OWNER_GROUP_ID = 2;
+
+        public static readonly string SHOP_GROUP_BU = "BU";
+        public static readonly string SHOP_GROUP_INDY = "IN";
+        public static readonly string SHOP_GROUP_SELLER = "SE";
+
+
+        public static readonly string LANG_EN = "EN";
+        public static readonly string LANG_TH = "TH";
+>>>>>>> d4c1fc45ca29bfa48acd2fc7ce2ef258322e225d
+    }
+}
---
+++ Colsp.Api/Constants/Constant_BASE_6932.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Colsp.Api.Constants
+{
+    public static class Constant
+    {
+        public static readonly string STATUS_ACTIVE = "AT";
+        public static readonly string STATUS_NOT_ACTIVE = "NA";
+        public static readonly string STATUS_REMOVE = "RM";
+        public static readonly string STATUS_VISIBLE = "VI";
+        public static readonly string STATUS_NOT_VISIBLE = "NV";
+
+        public static readonly string DATA_TYPE_STRING = "ST";
+        public static readonly string DATA_TYPE_LIST = "LT";
+
+        public static Dictionary<string, int?> STOCK_TYPE = new Dictionary<string, int?>() { {"Stock", 1 }, { "Pre-Order" , 2} };
+
+
+        public static readonly string PRODUCT_STATUS_DRAFT = "DF";
+        public static readonly string PRODUCT_STATUS_WAIT_FOR_APPROVAL = "WA";
+        public static readonly string PRODUCT_STATUS_JUNK = "JU";
+        public static readonly string PRODUCT_STATUS_APPROVE = "AP";
+        public static readonly string PRODUCT_STATUS_NOT_APPROVE = "RJ";
+
+
+        public static readonly string USER_TYPE_ADMIN = "A";
+        public static readonly string USER_TYPE_SELLER = "S";
+        public static readonly string SHOP_TYPE = "H";
+
+        public static readonly string COUPON_FILTER_INCLUDE = "I";
+        public static readonly string COUPON_FILTER_EXCLUDE = "E";
+
+        public static int SHOP_OWNER_GROUP_ID = 2;
+
+        public static string SHOP_GROUP_BU = "BU";
+        public static string SHOP_GROUP_INDY = "IN";
+        public static string SHOP_GROUP_SELLER = "SE";
+    }
+}
