974630095e82ea0af8a6488fb33e1e444a79230c
weerasit <naydeemak@gmail.com>
2016-03-15 14:22:22 +0700

    new class from laki

---
+++ Colsp.Model/Requests/CMSGroupRequest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Colsp.Model.Requests
+{
+    public class CMSGroupRequest
+    {
+        public string CMSGroupNameEN { get; set; }
+        public string CMSGroupNameTH { get; set; }
+        public string BannerLocation { get; set; }
+        public string BannerConntent { get; set; }
+        public int Sequence { get; set; }
+        public bool Status { get; set; }
+        public bool Visibility { get; set; }
+        public Nullable<int> CreateBy { get; set; }
+        public string CreateIP { get; set; }
+        public int? ShopId { get; set; }
+    }
+}
