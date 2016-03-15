b37fa5ae1e75fbdbb4155580d46861785049a996
weerasit <naydeemak@gmail.com>
2016-03-15 11:35:43 +0700

    fix

--- Colsp.Entity/Models/Inventory.cs
+++ Colsp.Entity/Models/Inventory.cs
@@ -29,5 +29,7 @@ namespace Colsp.Entity.Models
         public Nullable<System.DateTime> CreatedDt { get; set; }
         public string UpdatedBy { get; set; }
         public Nullable<System.DateTime> UpdatedDt { get; set; }
+    
+        public virtual ProductStage ProductStage { get; set; }
     }
 }
