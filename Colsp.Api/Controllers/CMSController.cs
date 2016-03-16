7e7f7a16d416e558e9d5ff8f65bacec25efcae10
นที พูลภักดี < >
2016-03-16 16:09:31 +0700

    add business logic layer

---
+++ Colsp.Api/Controllers/CMSController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Colsp.Api.Controllers
+{
+    public class CMSController : ApiController
+    {
+        
+        public HttpResponseMessage GetAllCMSCategory()
+        {
+            try
+            {
+                
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+            return null;
+        }
+
+    }
+}
---
+++ Colsp.Logic/CMSLogic.cs
@@ -0,0 +1,96 @@
+﻿using Colsp.Entity.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Colsp.Logic
+{
+    public class CMSLogic
+    {
+
+        #region Start CMS Category Method
+        // Get All CMS Category
+        public List<Object> GetAllCMSCategory()
+        {
+            List<Object> categorys = null;
+
+            try
+            {
+                using (ColspEntities db = new ColspEntities())
+                {
+                    
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+            return categorys;
+        }
+
+        // Add CMS Category
+        public bool AddCMSCategory()
+        {
+            bool success = false;
+
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+            return success;
+        }
+
+        // Edit CMS Category
+        public bool EditCMSCategory()
+        {
+            bool success = false;
+
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+            return success;
+        }
+
+        // Delete CMS Category
+        public bool DeleteCMSCategory()
+        {
+            bool success = false;
+
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+            return success;
+        }
+        #endregion
+
+
+        #region Start CMS Master Method
+
+        #endregion
+
+
+        #region Start CMS Group Method
+
+        #endregion
+    }
+}
