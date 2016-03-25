edf340527c0bf44636902788aa93c67456a99748
weerasit <naydeemak@gmail.com>
2016-03-25 16:49:54 +0700

    entity cms

---
+++ Colsp.Model/Requests/CMSCategoryRequest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Colsp.Model.Requests
+{
+    public class CMSCategoryRequest
+    {
+
+    }
+    public class MasterGetCategoryRequest:PaginatedRequest {
+        public bool? IsCampaign { get; set; }
+        public string SearchText { get; set; }
+    }
+
+}
---
+++ Colsp.Model/Requests/CMSSchedulerRequest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Colsp.Model.Requests
+{
+    public class CMSSchedulerRequest
+    {
+        public int CMSMasterId { get; set; }
+        public int CMSSchedulerId { get; set; }
+        public Nullable<System.DateTime> EffectiveDate { get; set; }
+        public Nullable<System.TimeSpan> EffectiveTime { get; set; }
+        public Nullable<System.DateTime> ExpiryDate { get; set; }
+        public Nullable<System.TimeSpan> ExpiryTime { get; set; }
+    }
+}
