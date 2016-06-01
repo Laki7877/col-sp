299e13c2623fc7a74cd74b638ac7985646243ccc
weerasit <naydeemak@gmail.com>
2016-06-01 10:01:20 +0700

    add file request

---
+++ Colsp.Model/Requests/ReturnReportRequest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Colsp.Model.Requests
+{
+    public class ReturnReportRequest : PaginatedRequest
+    {
+        public string OrderId { get; set; }
+        public string PID { get; set; }
+        public string ItemName { get; set; }
+        public string ItemStatus { get; set; }
+        public string ReturnDateFrom { get; set; }
+        public string ReturnDateTo { get; set; }
+        public ReturnReportRequest()
+        {
+            OrderId = string.Empty;
+            PID = string.Empty;
+            ItemName = string.Empty;
+            ItemStatus = string.Empty;
+            ReturnDateFrom = string.Empty;
+            ReturnDateTo = string.Empty;
+        }
+    }
+}
---
+++ Colsp.Model/Requests/SellerReportSaleRequest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Colsp.Model.Requests
+{
+    public class SellerReportSaleRequest : PaginatedRequest
+    {
+        public DateTime OrderDateFrom { get; set; }
+        public DateTime OrderDateEnd { get; set; }
+        public string ItemStatus { get; set; }
+        public int PID { get; set; }
+        public string ItemName { get; set; }
+        public int BrandId { get; set; }
+        public int GlobalCategoryId { get; set; }
+        public int LocalCategoryId { get; set; }
+        public SellerReportSaleRequest()
+        {
+            OrderDateFrom = new DateTime();
+            OrderDateEnd = new DateTime();
+            ItemStatus = string.Empty;
+            PID = 0;
+            ItemName = string.Empty;
+            BrandId = 0;
+            GlobalCategoryId = 0;
+            LocalCategoryId = 0;
+        }
+    }
+}
+
+   
